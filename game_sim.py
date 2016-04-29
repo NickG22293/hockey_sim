f6bbc659a2f0c2e9b3aa8047b164f1783d387176
Nick Gordon <ngordon22293@gmail.com>
2016-04-29 15:13:40 -0400

    Add various Stats classes

--- game_sim.py
+++ game_sim.py
@@ -7,25 +7,84 @@
 # so optimization will have to be kept in mind.
 
 from lxml import html, etree
+from bs4 import BeautifulSoup
 import requests
 
-# pull down info from stats site using lxml and requests
-def parse_hockey_stats(team):
-    # build the URL using Team
-    url = "http://www.hockey-reference.com/teams/" + team + "/2016.html"
-    page = requests.get(url)
-    # content is put in a nice tree format for xpath parsing
-    tree = html.fromstring(page.content)
-    # Xpath query for team's stats
-    stats = tree.xpath('//table[@id="team_stats"]/*/*')
-    rows = iter(stats)
-    for row in rows:
-        values = [col.text for col in row]
-        for c in values:
-            if c:
-                print 'Stats: ', c
+# Parent class to hold apropo stats for playoff/regular season
+class SeasonStats:
+    def __init__(self):
+        self.shots = 0      ## Total shots season
+        self.shotspg = 0    ## Shots per game
+        self.shotperc = 0   ## Shot % across team
+        self.svperc = 0     ## Save % across goalies
+        self.record = 0     ## Total record
+
+# Regular season stats
+class RegularSeasonStats(SeasonStats):
+    def __init__(self):
+        SeasonStats.__init__(self)
+# Post season stats
+class PostSeasonStats(SeasonStats):
+    def __init__(self):
+        SeasonStats.__init__(self)
+        self.seed = 0           #Seed coming into playoffs
+        self.playoff_gp = 0;    #Playoffs games played
+
+
+# Stats class to hold all relevant stats for the prediction for a single team
+# To be used in tandem with other TeamStats with the HockeySim methods
+class TeamStats:
+    def __init__(self):
+        self.team = ""
+        self.playoff_stats = PostSeasonStats()
+        self.regular_stats = RegularSeasonStats()
+
+    # Cols as follows:
+    # AvAge, GP, W, L, OL, PTS, PTS%, GF, GA, SRS, SOS
+    # TG/G, PPG, PPO, PP% PPA, PPO, PK%, SHG, SHA, S, S%
+    def fill_team_stats(self, table):
+        # fill the regular season stats
+        self.regular_stats.shots = table[21]
+        self.regular.shotspg = self.shots/82
+        self.regular.shotperc = table[22]
+        self.regular.svperc = table[24]
+        print_team_stats(self.regular_stats)
+
+    def print_team_stats(stats):
+        print "Shots: ", stats.shots
+        print "Shot %: ", stats.shotperc
+        print "Record: ", stats.record
+        print "Save %: ", stats.svperc
+        print "Shots PG: ", stats.shotspg
+        
+
+    # pull down info from stats site using lxml and requests
+    def parse_hockey_stats(self, team):
+        # build the URL using Team
+        url = "http://www.hockey-reference.com/teams/" + team + "/2016.html"
+        page = requests.get(url)
+        # content is put in a nice tree format for xpath parsing
+        tree = html.fromstring(page.content)
+        # Xpath query for team's stats
+        stats = tree.xpath('//table[@id="team_stats"]/tbody/*')
+        assert isinstance(stats, object)
+        # fill stats vars
+        table = iter(stats)
+        fill_team_stats(table)
+
+class HockeySim:
+    def __init__(self):
+        build_playoff_bracket()
+
+    def build_playoff_bracket(self):
+        #TODO: build playoff bracket
+        url = ""
 
 def main():
-    parse_hockey_stats('PIT')
+    home = TeamStats()
+    home.parse_hockey_stats('WSH')
+    away = TeamStats()
+    away.parse_hockey_stats('PIT')
+    print '\n'
 
 main()
